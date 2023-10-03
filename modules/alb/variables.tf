0678daa64132e29f68f2414d67e19094b478c904
ishita1712 <127949276+ishita1712@users.noreply.github.com>
2023-10-03 22:33:19 +0530

    Add files via upload

---
+++ modules/alb/variables.tf
@@ -0,0 +1,7 @@
+variable "ec2_instance" {}
+variable "project-name" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+
